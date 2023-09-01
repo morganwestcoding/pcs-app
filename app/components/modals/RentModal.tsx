84b7a13061172f4a62abbbe9572f17ce08448bd4
Morgan West <mewwes@gmail.com>
2023-08-31 23:44:30 -0700

    register modal

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -274,7 +274,7 @@ const RentModal = () => {
     <Modal
       disabled={isLoading}
       isOpen={rentModal.isOpen}
-      title="Airbnb your home!"
+      title="Share your service today!"
       actionLabel={actionLabel}
       onSubmit={handleSubmit(onSubmit)}
       secondaryActionLabel={secondaryActionLabel}
